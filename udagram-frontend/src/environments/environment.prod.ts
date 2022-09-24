154ed72083b0b0fbf20602a717b05e215b67e239
shinarasheed <rasheedshinaopeyemi@gmail.com>
2022-09-24 10:18:00 +0100

    recreated images repository and and recreated images and get them pushed to docker hub

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a34dd5bdc90784d17874ceb785f44344-2141786644.us-east-2.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
